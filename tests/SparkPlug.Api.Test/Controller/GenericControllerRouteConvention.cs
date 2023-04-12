54c4337599fe07c626e38c618f26b52bad9c6d7a
Natarajan Ganapathi <Natarajan_Ganapathi@epam.com>
2023-04-12 22:44:07 +0530

    unit test issue fix

--- tests/SparkPlug.Api.Test/Controller/GenericControllerRouteConvention.cs
+++ tests/SparkPlug.Api.Test/Controller/GenericControllerRouteConvention.cs
@@ -7,22 +7,15 @@ public class GenericControllerRouteConventionTests
     {
         // Arrange
         var convention = new GenericControllerRouteConvention(new RouteAttribute("{tenant}"), false);
-        var controllerModel = new ControllerModel(typeof(TestController).GetTypeInfo(), Array.Empty<object>());
+        var controllerModel = new ControllerModel(typeof(ApiController<long, User>).GetTypeInfo(), Array.Empty<object>());
 
         // Act
         convention.Apply(controllerModel);
 
         // Assert
-        // Assert.Single(controllerModel.Selectors);
-        // Assert.NotNull(controllerModel.Selectors[0].AttributeRouteModel);
-        // Assert.Equal("TModel", controllerModel.Selectors[0].AttributeRouteModel?.Template);
-    }
-
-    [ApiController, Route("test"), ApiExplorerSettings(GroupName = "v1")]
-    public class TestController : BaseController<long, User>
-    {
-        public TestController(IServiceProvider serviceProvider) : base(serviceProvider) { }
-        public static IActionResult? Index() => null;
+        Assert.Single(controllerModel.Selectors);
+        Assert.NotNull(controllerModel.Selectors[0].AttributeRouteModel);
+        Assert.Equal("user", controllerModel.Selectors[0].AttributeRouteModel?.Template);
     }
 
     [Api("user"), TenantDbEntity]
